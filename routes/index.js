8058920dd0552fae9c7eb7001a85dc92aa12b551
MarioPdr <mario.coctop@gmail.com>
2025-05-15 21:26:08 -0300

    Correções

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', {
-    dever: 'Dever de Casa',
-    tjade: 'Testes com a template engine Jade'
+  res.render('index', { title: 'Dever de casa',
+    paragrafo: 'Testes com a template engine Jade'
    });
 });
 
